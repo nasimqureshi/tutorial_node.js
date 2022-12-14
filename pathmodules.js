d973c75a0d0415a538092ab8ffbdfdd78825a601
nasimqureshi <qureshi.nasim@yahoo.com>
2022-12-14 12:05:46 -0600

    my commit

---
+++ pathmodules.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const a1 = path.basename('c:\\temp\\myfile.html');
+const a2 = path.basename('c:\\temp\\myfile.html');
+
+console.log(a1)
+console.log(a2)
+const a3 = path.extname(__filename)
+console.log(a3)
